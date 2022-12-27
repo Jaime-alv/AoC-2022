3bbfd47f6d7ffd25849158eac09d7b5e027aacdc
jaime-pue <jaime.alvarez@pue.es>
2022-12-27 17:19:55 +0100

    Update day 2

--- day 02/day_02.py
+++ day 02/day_02.py
@@ -68,6 +68,26 @@ class Day2:
             result += self.get_player_move(hand)
         return result
 
+    map_input = {'A': 'Rock', 'B': 'Paper', 'C': 'Scissors',
+                 'X': 'Lose', 'Y': 'Draw', 'Z': 'Win'}
+    points_per_shape = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
+    points_per_outcome = {'Lose': 0, 'Draw': 3, 'Win': 6}
+
+    def points_per_round2(self, move: str) -> int:
+        opponent_shape = self.map_input[move[0]]
+        our_goal = self.map_input[move[2]]
+
+        if (opponent_shape, our_goal) in [('Rock', 'Draw'), ('Paper', 'Lose'), ('Scissors', 'Win')]:
+            return self.points_per_outcome[our_goal] + self.points_per_shape['Rock']
+        elif (opponent_shape, our_goal) in [('Rock', 'Win'), ('Paper', 'Draw'), ('Scissors', 'Lose')]:
+            return self.points_per_outcome[our_goal] + self.points_per_shape['Paper']
+        else:
+            return self.points_per_outcome[our_goal] + self.points_per_shape['Scissors']
+
+    def part2(self) -> int:
+        return sum([self.points_per_round2(move) for move in self.data])
+
+
 class TestDay2(unittest.TestCase):
     example: list[str] = [
         'A Y',
@@ -85,6 +105,7 @@ class TestDay2(unittest.TestCase):
     def test_part_1(self):
         self.assertEqual(self.base.process_part_1(), 15)
         self.assertEqual(Day2(load_file(input_file)).process_part_1(), 11906)
+        self.assertEqual(Day2(load_file(input_file)).part2(), 11186)
 
 
 if __name__ == '__main__':
